a76ae98dd384632b15d9faf9592c2b99935dcc75
Niels Vandekeybus <progster@gmail.com>
2023-06-12 14:27:33 +0200

    update constants

--- constants.py
+++ constants.py
@@ -1,5 +1,16 @@
-RESOURCE_BASE = "http://data.lblod.info/"
+import os
+RESOURCE_BASE = "http://data.lblod.info/id/"
 SCRAPE_JOB_TYPE = "http://lblod.data.gift/ns/ScrapeJob"
 SCRAPE_CAMPAIGN_JOB_TYPE = "http://lblod.data.gift/ns/ScrapeCampaignJob"
 SCRAPE_GRAPH = "http://mu.semte.ch/graphs/jobs-graph"
-EXTRACTION_GRAPH = "http://mu.semte.ch/graphs/extracted-triples-graph"
+DEFAULT_GRAPH = os.environ['DEFAULT_GRAPH'] or 'http://mu.semte.ch/graphs/public'
+STATUSES =  {
+    'READY': 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached',
+    'ONGOING': 'http://lblod.data.gift/file-download-statuses/ongoing',
+    'SUCCESS': 'http://lblod.data.gift/file-download-statuses/success',
+    'FAILURE': 'http://lblod.data.gift/file-download-statuses/failure'
+}
+
+OPERATIONS = {
+    'COLLECTING': 'http://lblod.data.gift/id/jobs/concept/TaskOperation/collecting'
+}
